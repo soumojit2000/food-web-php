f3e012e57d6379968fd66e748545872df1afc10c
soumojit@2000 <soumojitdutta0542@gmail.com>
2025-11-27 13:40:44 +0530

    updated css for styling

--- home.php
+++ home.php
@@ -49,7 +49,7 @@ unset($_SESSION['error_msg']);
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- custom css -->
-    <link rel="stylesheet" href="main.css?v=1" />
+    <link rel="stylesheet" href="main.css" />
     
   </head>
   <body>
